85fcf68556a45e4a8e3701d1c767cf8fce7e8e08
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2025-11-05 13:06:40 +0700

    Finalize the app

---
+++ lib/actions/booking.actions.ts
@@ -0,0 +1,18 @@
+'use server';
+
+import Booking  from "@/database/booking.model";
+import connectDB from "../mongodb";
+
+export const createBooking = async ( {email, slug, eventId} : {eventId: string; slug: string; email: string }) => {
+    try {
+        await connectDB();
+
+        const booking = (await Booking.create({eventId, slug, email})).lean();
+
+        return { success: true, booking };
+
+    } catch (error) {
+        console.error('Create booking failed', error);
+        return {success: false, error: error };
+    }
+}
