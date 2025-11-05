e44bdbbd3cf480bdfd1351667247151ac80fcf60
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2025-11-05 13:44:38 +0700

    Fix base url

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@ import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
 import { IEvent } from '@/database';
 import { cacheLife } from 'next/cache';
-import { events } from '@/lib/constant';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache';
     cacheLife('hours');
-    // const response = await fetch(`${BASE_URL}/api/events`);
-    // const { events } = await response.json();
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
     return (
         <section>
@@ -27,7 +26,7 @@ const Page = async () => {
                 <h3>Featured Events</h3>
 
                 <ul className='events list-none'>
-                    {events && events.length > 0 && events.map((event) => (
+                    {events && events.length > 0 && events.map((event: IEvent) => (
                         <li key={event.title} >
                             <EventCard {...event}></EventCard>
                         </li>
